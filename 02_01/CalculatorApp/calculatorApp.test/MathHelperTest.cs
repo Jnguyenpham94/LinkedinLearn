34b5dfa16a10900c7a5e79b21c6dbbb61732fe52
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2021-11-20 20:27:54 -0800

    calculator test stuff DONE

--- 02_01/CalculatorApp/calculatorApp.test/MathHelperTest.cs
+++ 02_01/CalculatorApp/calculatorApp.test/MathHelperTest.cs
@@ -20,5 +20,63 @@ namespace calculatorApp.test
             Assert.False(xResult);
             Assert.True(yResult);
         }
+
+        [Theory]
+        [InlineData(1,2,1)]
+        [InlineData(1, 3, 2)]
+        public void diffTest(int x, int y, int expectedValue)
+        {
+            var calculator = new MathFormulas();
+            var result = calculator.Diff(x, y);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData(new int[3] {1,2,3}, 6)]
+        [InlineData(new int[3] {-4,-6,10}, 0)]
+        public void sumTest(int[] values, int expectedValue)
+        {
+            var calculator = new MathFormulas();
+            var result = calculator.Sum(values);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-4, -6, -10)]
+        public void addTest(int x, int y, int expectedValue)
+        {
+            var calculator = new MathFormulas();
+            var result = calculator.Add(x, y);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData(new int[3] { 1, 2, 3 }, 2)]
+        [InlineData(new int[3] { 20, -6, 10 }, 8)]
+        public void averageTest(int[] values, int expectedValue)
+        {
+            var calculator = new MathFormulas();
+            var result = calculator.Average(values);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(MathFormulas.Data), MemberType = typeof(MathFormulas))]
+        public void Add_MemberData_Test(int x, int y, int expectedValue)
+        {
+            var calculator = new MathFormulas();
+            var result = calculator.Add(x, y);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory(Skip ="This is just the reason...")]
+        [ClassData(typeof(MathFormulas))]
+        public void Add_ClassData_Test(int x, int y, int expectedValue)
+        {
+            var calculator = new MathFormulas();
+            var result = calculator.Add(x, y);
+            Assert.Equal(expectedValue, result);
+        }
     }
 }
